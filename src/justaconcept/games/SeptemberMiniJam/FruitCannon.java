4b2b170ba1df4d66d458319daa4a0d97ababc3be
Adam Streck <adam.streck@gmail.com>
2013-09-14 17:45:19 +0200

    Adding tomato controls.

--- src/justaconcept/games/SeptemberMiniJam/FruitCannon.java
+++ src/justaconcept/games/SeptemberMiniJam/FruitCannon.java
@@ -15,7 +15,7 @@ public class FruitCannon extends PApplet {
 	float direction_;
 	float speed_;
 	PImage sprite_;
-	
+
 	public SeedProjectile(float x_, float y_, float direction_, float speed_, PImage sprite_) {
 	    this.x_ = x_ - sprite_.width / 2;
 	    this.y_ = y_ - sprite_.height / 2;
@@ -25,25 +25,65 @@ public class FruitCannon extends PApplet {
 	}
 
 	void draw() {
+	    move();
 	    displayRotated(sprite_, direction_, x_, y_);
 	}
-	
+
 	void move() {
+	    x_ += speed_ * Math.sin(direction_);
+	    y_ -= speed_ * Math.cos(direction_);
+	}
+    }
+
+    class Tomato {
+	float x_;
+	float y_;
+	float direction_;
+	float speed_;
+	int state;
+	ArrayList<PImage> sprites_;
+
+	public Tomato(float x_, float y_, float direction_, float speed_, ArrayList<PImage> sprites_) {
+	    this.x_ = x_ - sprites_.get(0).width / 2;
+	    this.y_ = y_ - sprites_.get(0).height / 2;
+	    this.direction_ = direction_;
+	    this.speed_ = speed_;
+	    this.sprites_ = sprites_;
+	    state = 0;
+	}
+
+	public void draw() {
+	    println(String.valueOf(x_));
+	    displayRotated(sprites_.get(state), direction_, x_, y_);
+	}
+
+	public void move() {
+
+	}
+	
+	public void leftKey() {
+	    
+	}
+	
+	public void rightKey() {
 	    
 	}
     }
-    
-    final int WIDHT = 1000;
+
+    final int WIDTH = 1000;
     final int HEIGHT = 1000;
 
     int score_one = 0;
     int score_two = 0;
     PFont font;
-    PImage melon;
-    PImage seed_img;
+    PImage melon_img, banana_img, seed_img;
+    ArrayList<PImage> tomato_imgs;
+    final int TOMATO_STATE_COUNT = 3;
 
     final float STEP_WIDTH = 25.f;
     final float DEFAULT_ROTATION_STEP = 0.015f;
+    final float DEFAULT_PROJECTILE_SPEED = 0.85f;
+    final float DEFAULT_TOMATO_SPEED = 0.85f;
     float center_x;
     float center_y;
 
@@ -52,30 +92,42 @@ public class FruitCannon extends PApplet {
 
     float melon_rotation;
     float rotation_step = DEFAULT_ROTATION_STEP;
-    
+
+    Tomato tomato;
     ArrayList<SeedProjectile> projectiles;
 
     public void loadAssets() {
 	font = loadFont("Ziggurat.vlw");
 	textFont(font, 32);
-	melon = loadImage("melon.png");
+	melon_img = loadImage("fruit_melon.png");
+	banana_img = loadImage("fruit_banana.png");
 	seed_img = loadImage("seed.png");
-	
+	tomato_imgs = new ArrayList<PImage>();
+        tomato_imgs.add(loadImage("veg_tomatoFull.png"));
+        tomato_imgs.add(loadImage("veg_tomatoHalf.png"));
+        tomato_imgs.add(loadImage("veg_tomatoQuarter.png"));
+
 	projectiles = new ArrayList<SeedProjectile>();
     }
 
     public void setPlanetPos() {
-	center_x = WIDHT / 2 - melon.width / 2;
-	center_y = HEIGHT / 2 - melon.height / 2;
+	center_x = WIDTH / 2;
+	center_y = HEIGHT / 2;
+    }
+
+    void spawnTomato() {
+	float angle = random(PI * 2);
+	tomato = new Tomato(WIDTH / 2f * (float) Math.sin(angle), HEIGHT / 2f * (float) Math.cos(angle), angle, DEFAULT_TOMATO_SPEED, tomato_imgs);
     }
 
     @Override
     public void setup() {
-	size(WIDHT, HEIGHT);
+	size(WIDTH, HEIGHT);
 	loadAssets();
 	setPlanetPos();
+	spawnTomato();
     }
-    
+
     void displayRotated(PImage object, float rotation, float x, float y) {
 	pushMatrix();
 	translate(x, y);
@@ -85,7 +137,7 @@ public class FruitCannon extends PApplet {
 	image(object, 0, 0);
 	popMatrix();
     }
-    
+
     void displayProjectiles() {
 	for (SeedProjectile projectile : projectiles) {
 	    projectile.draw();
@@ -96,33 +148,43 @@ public class FruitCannon extends PApplet {
 	noStroke();
 	fill(0);
 	text("Score: " + score_one, 60, 40);
-	text("Score: " + score_two, WIDHT - 215, 40);
+	text("Score: " + score_two, WIDTH - 215, 40);
     }
-    
+
     void readKeys() {
 	if (keyPressed) {
 	    if (keyCode == LEFT) {
 		melon_rotation -= rotation_step;
 	    } else if (keyCode == RIGHT) {
 		melon_rotation += rotation_step;
-	    }
+	    } 
 	    
-	    if (keyCode == UP ) {
-		AffineTransform transform = AffineTransform.getRotateInstance(melon_rotation, 0, melon.height/2);
-		projectiles.add(new SeedProjectile((float) transform.getTranslateX() + melon.width/2, (float) transform.getTranslateY(), melon_rotation, 0f, seed_img));
+	    if (key == 'a') {
+		tomato.leftKey();
+	    } else if (key == 'd') {
+		tomato.rightKey();
+	    }
+
+	    if (key == ' ') {
+		AffineTransform transform = AffineTransform.getRotateInstance(melon_rotation, 0, melon_img.height / 2);
+		projectiles.add(new SeedProjectile((float) transform.getTranslateX(), (float) transform.getTranslateY() - (melon_img.height / 2), melon_rotation,
+			DEFAULT_PROJECTILE_SPEED, seed_img));
 	    }
 	}
     }
 
     @Override
     public void draw() {
+	// println("FrameRate: " + String.valueOf(frameRate));
 
 	background(255);
 
 	writeScore();
 	translate(center_x, center_y);
-	
-	displayRotated(melon, melon_rotation, 0f, 0f);
+
+	image(melon_img, -melon_img.width / 2f, -melon_img.height / 2f);
+	displayRotated(banana_img, melon_rotation, -banana_img.width / 2f, -banana_img.height / 2f);
+	tomato.draw();
 	displayProjectiles();
 
 	readKeys();
